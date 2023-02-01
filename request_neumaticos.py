3897192afd9ce890163ad1960fc9ef2883506d21
Santiago Suárez <santiaggosuarez@gmail.com>
2023-02-01 09:37:50 -0300

    Update request_neumaticos.py

--- request_neumaticos.py
+++ request_neumaticos.py
@@ -14,6 +14,8 @@ if __name__ == "__main__":
   list_result_ids = []
   list_permalink = []
   list_titles = []
+  list_tire_size = []
+  list_rim_diameter = []
   
   # Si la página no está caída sigo realizando consultas (si da 400 por ej es ERROR)
   if data.status_code == 200:
@@ -40,11 +42,31 @@ if __name__ == "__main__":
       title = data["title"]
       list_permalink.append(permalink)
       list_titles.append(title)
+
+      attributes = data["attributes"]
+      bool_check_size = True
+      for att in attributes:
+        if att["id"] == "MANUFACTURER_TIRE_SIZE":
+          bool_check_size = False
+          size = att["value_name"]
+          list_tire_size.append(size)
+      if bool_check_size:
+        list_tire_size.append("None")
+
+      attributes = data["attributes"]
+      bool_check_rim = True
+      for att in attributes:
+        if att["id"] == "RIM_DIAMETER":
+          bool_check_rim = False
+          rim = att["value_name"]
+          list_rim_diameter.append(rim)
+      if bool_check_rim:
+        list_rim_diameter.append("None")
           
   
   # Esta linea convierte dos listas a diccionario, no la uso por el momento
   dict_ids_and_permalink = dict(zip(list_result_ids, list_permalink))
   
   # Imprimo cada dato en las listas separados por comas para copiarlo en formato csv
-  for id, link, title in zip(list_result_ids, list_permalink, list_titles):
-    print(id, link, title, sep=",")
+  for id, link, title, size, diameter in zip(list_result_ids, list_permalink, list_titles, list_tire_size, list_rim_diameter):
+    print(id, link, title, size, diameter, sep=",")
